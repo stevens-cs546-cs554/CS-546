83640b9b5af38d9f71bb53d3c3467af48fa10b63
Phil Barresi <me@philbarresi.com>
2019-07-14 23:16:26 -0400

    Partially updated lecture 10

--- Lectures/lecture_08/code/data/index.js
+++ Lectures/lecture_08/code/data/index.js
@@ -1,11 +1,3 @@
-const postRoutes = require("./posts");
-const userRoutes = require("./users");
-
-let constructorMethod = app => {
-  app.use("/post", postRoutes);
-  app.use("/user", userRoutes);
-};
-
 module.exports = {
   users: require("./users"),
   posts: require("./posts")
