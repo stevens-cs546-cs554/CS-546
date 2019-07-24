7a08908c9ac7bacce6665f6afb42fa10bd6faf64
Patrick Hill <graffixnyc@gmail.com>
2019-07-24 14:02:57 -0400

    updated lecture 6 to async/await

--- Lectures/lecture_06/code/data/users.js
+++ Lectures/lecture_06/code/data/users.js
@@ -3,61 +3,54 @@ const users = mongoCollections.users;
 const uuid = require('uuid');
 
 let exportedMethods = {
-  getAllUsers() {
-    return users().then((userCollection) => {
-      return userCollection.find({}).toArray();
-    });
+  async getAllUsers() {
+    const userCollection = await users();
+    const userList = await userCollection.find({}).toArray();
+    return userList;
   },
   // This is a fun new syntax that was brought forth in ES6, where we can define
   // methods on an object with this shorthand!
-  getUserById(id) {
-    return users().then((userCollection) => {
-      return userCollection.findOne({_id: id}).then((user) => {
-        if (!user) throw 'User not found';
-        return user;
-      });
-    });
+  async getUserById(id) {
+    const userCollection = await users();
+    const user = await userCollection.findOne({_id: id});
+    if (!user) throw 'User not found';
+    return user;
   },
-  addUser(firstName, lastName) {
-    return users().then((userCollection) => {
-      let newUser = {
-        firstName: firstName,
-        lastName: lastName,
-        _id: uuid.v4()
-      };
+  async addUser(firstName, lastName) {
+    const userCollection = await users();
 
-      return userCollection
-        .insertOne(newUser)
-        .then((newInsertInformation) => {
-          return newInsertInformation.insertedId;
-        })
-        .then((newId) => {
-          return this.getUserById(newId);
-        });
-    });
-  },
-  removeUser(id) {
-    return users().then((userCollection) => {
-      return userCollection.removeOne({_id: id}).then((deletionInfo) => {
-        if (deletionInfo.deletedCount === 0) {
-          throw `Could not delete user with id of ${id}`;
-        }
-      });
-    });
-  },
-  updateUser(id, firstName, lastName) {
-    return this.getUserById(id).then((currentUser) => {
-      let updatedUser = {
-        firstName: firstName,
-        lastName: lastName
-      };
+    let newUser = {
+      firstName: firstName,
+      lastName: lastName,
+      _id: uuid.v4()
+    };
 
-      return users().then((userCollection) => {
-        return userCollection.updateOne({_id: id}, {$set: updatedUser}).then(() => {
-          return this.getUserById(id);
-        });
-      });
-    });
+    const newInsertInformation = await userCollection.insertOne(newUser);
+    if (newInsertInformation.insertedCount === 0) throw 'Insert failed!';
+    return await this.getUserById(newInsertInformation.insertedId);
+  },
+  async removeUser(id) {
+    const userCollection = await users();
+    const deletionInfo = await userCollection.removeOne({_id: id});
+    if (deletionInfo.deletedCount === 0) {
+      throw `Could not delete user with id of ${id}`;
+    }
+    return true;
+  },
+  async updateUser(id, firstName, lastName) {
+    const user = await this.getUserById(id);
+    console.log(user);
+
+    const userUpdateInfo = {
+      firstName: firstName,
+      lastName: lastName
+    };
+
+    const userCollection = await users();
+    const updateInfo = await userCollection.updateOne({_id: id}, {$set: userUpdateInfo});
+    if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw 'Update failed';
+
+    return await this.getUserById(id);
   }
 };
 
