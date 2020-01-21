f9d74ad7fd46640db1bf440d0bb537b6556ea4c8
Patrick Hill <graffixnyc@gmail.com>
2020-01-20 22:58:34 -0500

    Lecture 1 code update

--- lecture_01/code/arrays.js
+++ lecture_01/code/arrays.js
@@ -1,5 +1,16 @@
 let myStringArray = [ 'hello', 'world', 'my', 'name', 'is', 'Patrick' ];
 let myNumArray = [ 1, 2, 3, 4, 5 ];
+let mixedArray = [
+	1,
+	'Hello',
+	undefined,
+	true,
+	(message) => {
+		console.log(message);
+	}
+];
+
+mixedArray[4]('Hello world!');
 
 myStringArray.forEach((value) => {
 	console.log(value);
@@ -9,14 +20,15 @@ let myNumArraySquared = myNumArray.map((x) => {
 	return x * x;
 });
 
-console.log(myNumArray);
+// console.log(myNumArray);
 console.log(myNumArraySquared);
+console.log(myNumArray);
 
 let oddNumbers = myNumArray.filter((num) => {
 	return num % 2 === 1;
 });
 
-console.log(myNumArray);
+// console.log(myNumArray);
 console.log(oddNumbers);
 
 let sumOfOdds = oddNumbers.reduce((currentTotal, newValue) => {
@@ -26,5 +38,12 @@ let sumOfOdds = oddNumbers.reduce((currentTotal, newValue) => {
 
 console.log(sumOfOdds);
 
+console.log(myNumArray);
 myNumArray.push(6);
-console.log(myNumArray.join(' '));
+console.log(myNumArray);
+myNumArray.push('Patrick');
+console.log(myNumArray);
+console.log(myNumArray.pop());
+console.log(myNumArray);
+
+console.log(myNumArray.join('&&'));
--- lecture_01/code/objects.js
+++ lecture_01/code/objects.js
@@ -1,20 +1,36 @@
+let myBlankObj = {};
+
 let myObj = {
-  hello: "World",
-  num: 1,
-  bool: true,
-  myFn: () => {
-    return "Hello";
-  }
+	hello: 'World',
+	num: 1,
+	bool: true,
+	myFn: (message) => {
+		return message;
+	}
 };
 
 console.log(myObj);
 
-myObj["new-Key1"] = "I am a new key!";
+myObj['new-Key1'] = 'I am a new key!';
+console.log(myObj);
+
 myObj.directlyAddedKey = "I've been added!";
+console.log(myObj);
 
-let keyName = "myStrKey";
-myObj[keyName] = "This was made dynamically";
+let keyName = 'myStrKey';
+myObj[keyName] = 'This was made dynamically';
+console.log(myObj);
 
-myObj.hello = "Hello, world!";
+myObj.hello = 'Hello, world!';
+console.log(myObj);
 
-console.log(myObj.myFn());
+console.log(myObj.myFn('Hey Patrick'));
+
+const myConstObj = { a: 1, b: 2, c: 3 };
+console.log(myConstObj);
+myConstObj.c = 4;
+console.log(myConstObj);
+myConstObj.d = 6;
+console.log(myConstObj);
+// myConstObj = {};
+// console.log(myConstObj);
