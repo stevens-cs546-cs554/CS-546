8aeee0c09df2a63cdcc24e70a798b1f3b24fa6c6
Patrick Hill <graffixnyc@gmail.com>
2020-04-17 17:29:24 -0400

    added simple private route example

---
+++ lecture_10/code/private_route_example/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const configRoutes = require('./routes');
+
+app.use(express.json());
+
+app.use(
+	session({
+		name: 'AwesomeWebApp',
+		secret: "This is a secret.. shhh don't tell anyone",
+		saveUninitialized: true,
+		resave: false
+	})
+);
+
+app.use('/private', (req, res, next) => {
+	if (!req.session.user) {
+		return res.redirect('/');
+	} else {
+		next();
+	}
+});
+
+app.use('/login', (req, res, next) => {
+	if (req.session.user) {
+		return res.redirect('/private');
+	} else {
+		//here I',m just manually setting the req.method to post since it's usually coming from a form
+		req.method = 'POST';
+		next();
+	}
+});
+
+configRoutes(app);
+
+app.listen(3000, () => {
+	console.log("We've now got a server!");
+	console.log('Your routes will be running on http://localhost:3000');
+});
---
+++ lecture_10/code/private_route_example/routes/index.js
@@ -0,0 +1,15 @@
+const postRoutes = require('./posts');
+const userRoutes = require('./users');
+const privateRoutes = require('./private');
+
+const constructorMethod = (app) => {
+	app.use('/', userRoutes);
+	app.use('/posts', postRoutes);
+	app.use('/private', privateRoutes);
+
+	app.use('*', (req, res) => {
+		res.sendStatus(404);
+	});
+};
+
+module.exports = constructorMethod;
