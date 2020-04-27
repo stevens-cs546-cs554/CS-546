7924b6fffc7e1f2d174b8eb4bea6ce8ac0df66f3
Patrick Hill <graffixnyc@gmail.com>
2020-04-27 13:37:13 -0400

    Updated Lecture 11 code

--- lecture_11/code/data/index.js
+++ lecture_11/code/data/index.js
@@ -2,68 +2,69 @@ let currId = 0;
 let todoListEntries = {};
 
 let makeToDo = function(title, task) {
-  if (!title) throw "Must provide a title";
-  if (!task) throw "Must provide a task";
+	if (!title) throw 'Must provide a title';
+	if (!task) throw 'Must provide a task';
 
-  let newTask = {
-    id: ++currId,
-    title: title,
-    task: task,
-    done: false,
-    notDone: true
-  };
+	let newTask = {
+		id: ++currId,
+		title: title,
+		task: task,
+		done: false,
+		notDone: true
+	};
 
-  todoListEntries[newTask.id] = newTask;
-  return newTask;
+	todoListEntries[newTask.id] = newTask;
+	return newTask;
 };
 
 let getToDo = function(id) {
-  if (!todoListEntries[id]) throw "No such entry exists";
-  return todoListEntries[id];
+	if (!todoListEntries[id]) throw 'No such entry exists';
+	return todoListEntries[id];
 };
 
 let finishToDo = function(id) {
-  let entry = getToDo(id);
-  entry.done = true;
-  entry.notDone = false;
+	let entry = getToDo(id);
+	entry.done = true;
+	entry.notDone = false;
 
-  return entry;
+	return entry;
 };
 
 let updateToDo = function(id, newTitle, newTask) {
-  let entry = getToDo(id);
-  if (newTitle) entry.title = newTitle;
-  if (newTask) entry.task = newTask;
+	let entry = getToDo(id);
+	if (newTitle) entry.title = newTitle;
+	if (newTask) entry.task = newTask;
 
-  return entry;
+	return entry;
 };
 
 let getAll = function() {
-  return Object.keys(todoListEntries).map(function(key) {
-    return todoListEntries[key];
-  });
+	return Object.keys(todoListEntries).map(function(key) {
+		return todoListEntries[key];
+	});
 };
 
 let getFinished = function() {
-  return getAll().filter(function(entry) {
-    return entry.done;
-  });
+	return getAll().filter(function(entry) {
+		return entry.done;
+	});
 };
 
 let getUnfinished = function() {
-  return getAll().filter(function(entry) {
-    return !entry.done;
-  });
+	return getAll().filter(function(entry) {
+		return !entry.done;
+	});
 };
 
 module.exports = {
-  getToDo: getToDo,
-  finishToDo: finishToDo,
-  updateToDo: updateToDo,
-  getAll: getAll,
-  getFinished: getFinished,
-  getUnfinished: getUnfinished,
-  makeToDo: makeToDo
+	getToDo: getToDo,
+	finishToDo: finishToDo,
+	updateToDo: updateToDo,
+	getAll: getAll,
+	getFinished: getFinished,
+	getUnfinished: getUnfinished,
+	makeToDo: makeToDo
 };
 
-module.exports.makeToDo("Finish ToDo List", "You should finish this list");
+module.exports.makeToDo('Take Out The Trash', "Don't forget to take out the trash every Sunday and Wednesday night");
+module.exports.makeToDo('Pay Cable Bill', 'Pay on the 15th of the month');
--- lecture_11/code/routes/api.js
+++ lecture_11/code/routes/api.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const todoData = require("../data");
-const xss = require("xss");
+const todoData = require('../data');
+const xss = require('xss');
 
-router.post("/todo", function(request, response) {
-  todoData.makeToDo(request.body.name, request.body.description);
+router.post('/todo', function(request, response) {
+	todoData.makeToDo(xss(request.body.name), xss(request.body.description));
 
-  // response.json({ success: true, message: request.body.description });
-  response.json({ success: true, message: xss(request.body.description) });
+	// response.json({ success: true, message: request.body.description });
+	response.json({ success: true, message: xss(request.body.description) });
 });
 
-router.post("/todo/complete/:id", function(request, response) {
-  const updatedData = todoData.finishToDo(parseInt(request.params.id));
-  response.render("partials/todo_item", { layout: null, ...updatedData });
+router.post('/todo/complete/:id', function(request, response) {
+	const updatedData = todoData.finishToDo(parseInt(request.params.id));
+	response.render('partials/todo_item', { layout: null, ...updatedData });
 });
 
 /*
@@ -24,13 +24,10 @@ router.post("/todo.html", function(request, response) {
   response.send("<div>" + xss(request.body.description) + "</div>");
 });*/
 
-router.post("/todo.html", function(request, response) {
-  const newTodo = todoData.makeToDo(
-    request.body.name,
-    request.body.description
-  );
+router.post('/todo.html', function(request, response) {
+	const newTodo = todoData.makeToDo(request.body.name, request.body.description);
 
-  response.render("partials/todo_item", { layout: null, ...newTodo });
+	response.render('partials/todo_item', { layout: null, ...newTodo });
 });
 
 module.exports = router;
