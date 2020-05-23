316597695f03805088c03ed59946c8e98f0295b3
Patrick Hill <graffixnyc@gmail.com>
2020-05-22 20:07:08 -0400

    New semester reset

--- lecture_06/code/intermediate_api/app.js
+++ lecture_06/code/intermediate_api/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const configRoutes = require('./routes');
 
-app.use(cors());
 app.use(express.json());
 
 configRoutes(app);
 
 app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+	console.log("We've now got a server!");
+	console.log('Your routes will be running on http://localhost:3000');
 });
--- lecture_09/code/Lecture_Code/app.js
+++ lecture_09/code/Lecture_Code/app.js
@@ -5,20 +5,28 @@ const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
 
 const handlebarsInstance = exphbs.create({
-  defaultLayout: 'main'
+	defaultLayout: 'main',
+	// Specify helpers which are only registered on this instance.
+	helpers: {
+		asJSON: (obj, spacing) => {
+			if (typeof spacing === 'number') return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+
+			return new Handlebars.SafeString(JSON.stringify(obj));
+		}
+	}
 });
 
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
-  // If the user posts to the server with a property called _method, rewrite the request's method
-  // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
-  // rewritten in this middleware to a PUT route
-  if (req.body && req.body._method) {
-    req.method = req.body._method;
-    delete req.body._method;
-  }
+	// If the user posts to the server with a property called _method, rewrite the request's method
+	// To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+	// rewritten in this middleware to a PUT route
+	if (req.body && req.body._method) {
+		req.method = req.body._method;
+		delete req.body._method;
+	}
 
-  // let the next middleware run:
-  next();
+	// let the next middleware run:
+	next();
 };
 
 app.use;
@@ -33,6 +41,6 @@ app.set('view engine', 'handlebars');
 configRoutes(app);
 
 app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+	console.log("We've now got a server!");
+	console.log('Your routes will be running on http://localhost:3000');
 });
