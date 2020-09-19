cbf8264715222af15e2c20380cfea862c2aabd5f
Patrick Hill <graffixnyc@gmail.com>
2020-09-19 19:18:42 -0400

    Added another async/await example

---
+++ lecture_03/code/another-async-await-example/app.js
@@ -0,0 +1,15 @@
+const getData = require('./getdata');
+
+async function main() {
+	//try removing the await keyword and run the application
+
+	console.log(await getData.getApiData());
+}
+
+main();
+
+/*	this console.log will not be blocked
+		as it does not depend on the results
+		of main so it will execute before 
+		main is finished */
+console.log('after main is run');
--- lecture_03/code/async-await/app.js
+++ lecture_03/code/async-await/app.js
@@ -5,37 +5,39 @@ const fs = bluebird.promisifyAll(require('fs'));
 
 // We declare an async function that we will run below, so that we may use await.
 async function main() {
-  const getFileOperation = {
-    name: 'fileName',
-    description: 'What file do you want to open?'
-  };
+	const getFileOperation = {
+		name: 'fileName',
+		description: 'What file do you want to open?'
+	};
 
-  // Gets result of user input
-  let promptResult = await prompt.getAsync([getFileOperation]);
-  const fileName = promptResult.fileName;
+	// Gets result of user input
+	let promptResult = await prompt.getAsync([ getFileOperation ]);
+	console.log(promptResult);
+	const fileName = promptResult.fileName;
 
-  if (!fileName) {
-    throw 'Need to provide a file name';
-  }
+	if (!fileName) {
+		throw 'Need to provide a file name';
+	}
 
-  console.log(`About to read ${fileName} if it exists`);
-  const fileContent = await fs.readFileAsync(fileName, 'utf-8');
+	console.log(`About to read ${fileName} if it exists`);
 
-  const reversedContent = fileContent.split('').reverse().join('');
+	const fileContent = await fs.readFileAsync(fileName, 'utf-8');
 
-  const reversedName = `reversed_${fileName}`;
+	const reversedContent = fileContent.split('').reverse().join('');
 
-  // Now we have the actual file data read
+	const reversedName = `reversed_${fileName}`;
 
-  await fs.writeFileAsync(reversedName, reversedContent);
-  console.log('Finished!');
+	// Now we have the actual file data read
+	console.log('before write');
+	await fs.writeFileAsync(reversedName, reversedContent);
+	console.log('Finished!');
 
-  return null;
+	return null;
 }
 
 // Now we run it
 main().catch((err) => {
-  console.log(err);
+	console.log(err);
 });
 
-console.log('After prompt is run');
+console.log('After prompt');
