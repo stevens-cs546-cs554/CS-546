6232a09bd64e8b16fa338db576fb9bcd30a862e4
Patrick Hill <graffixnyc@gmail.com>
2020-10-31 15:35:36 -0400

    lecture 9 update

--- lecture_09/code/Simple_DOM_Example/script.js
+++ lecture_09/code/Simple_DOM_Example/script.js
@@ -1,61 +1,68 @@
-let myForm = document.getElementById('myForm');
-let textInput = document.getElementById('text_input');
-let errorDiv = document.getElementById('error');
-let myUl = document.getElementById('list');
-if (myForm) {
-  myForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    if (textInput.value) {
-      errorDiv.hidden = true;
-      let li = document.createElement('li');
-      li.innerHTML = textInput.value;
-      myUl.appendChild(li);
-      myForm.reset();
-      textInput.focus();
-    } else {
-      errorDiv.hidden = false;
-      errorDiv.innerHTML = 'You Must Enter a value!';
-      textInput.focus();
-    }
-  });
+// let myP = document.getElementById("myP")
+// myP.innerHTML= 'I have been changed'
+// let myBtnTwo = document.getElementById('myBtnTwo')
+// let myErrorDiv = document.getElementById("error")
+// let myDivBtn = document.getElementById("divBtn")
+// let divContainer = document.getElementById("container")
+// let myUl = document.getElementById("list")
+
+// function btnClick(){
+// 	if (myP.className == 'myClassOne'){
+// 		myP.className = 'myClassTwo'
+// 		myP.innerHTML= "I am now classTwo"
+// 	}else{
+// 		myP.className = 'myClassOne'
+// 		myP.innerHTML= "I am now classOne"
+// 	}
+// }
+
+
+// myBtnTwo.addEventListener('click', ()=>{
+// 	if (myErrorDiv.hidden== true){
+// 		myErrorDiv.hidden=false
+// 	}else{
+// 		myErrorDiv.hidden= true
+// 	}
+// })
+
+// myDivBtn.addEventListener('click', ()=>{
+// 	let newP = document.createElement('p')
+// 	newP.innerHTML = "I'm another P tag"
+// 	divContainer.appendChild(newP)
+// })
+
+// function addItem(){
+// 	let li=document.createElement("li")
+// 	li.innerHTML = "I'm a new item"
+// 	myUl.appendChild(li)
+// }
+
+let myForm = document.getElementById("myForm")
+let textInput = document.getElementById("text_input")
+let errorDiv = document.getElementById("error")
+let myUl = document.getElementById("list")
+let frmLabel = document.getElementById("formLabel")
+
+if (myForm){
+	myForm.addEventListener('submit', (event)=>{
+		event.preventDefault()
+		if (textInput.value.trim()){
+			textInput.classList.remove("inputClass")
+			errorDiv.hidden=true
+			frmLabel.classList.remove ("error")
+			let li = document.createElement("li")
+			li.innerHTML=textInput.value
+			myUl.appendChild(li)
+			myForm.reset ()
+			textInput.focus()
+		}else{
+			textInput.value=""
+			errorDiv.hidden=false
+			errorDiv.innerHTML= "You must enter a value"
+			frmLabel.className= "error"
+			textInput.focus()
+			textInput.className="inputClass"
+			
+		}
+	})
 }
-
-// let myP = document.getElementById('myP');
-// myP.innerHTML = 'I have been changed';
-// let myBtnTwo = document.getElementById('myBtnTwo');
-// let myDiv = document.getElementById('error');
-// let divBtn = document.getElementById('divBtn');
-// let divContainer = document.getElementById('container');
-// let ul = document.getElementById('list');
-
-// function btnClick() {
-// 	if (myP.className == 'myClassOne') {
-// 		myP.className = 'myClassTwo';
-// 		myP.innerHTML = 'I am now class Two';
-// 	} else {
-// 		myP.className = 'myClassOne';
-// 		myP.innerHTML = 'I am now class One';
-// 	}
-// }
-
-// myBtnTwo.addEventListener('click', () => {
-// 	//alert('btn two has been clicked');
-
-// 	if (myDiv.hidden == true) {
-// 		myDiv.hidden = false;
-// 	} else {
-// 		myDiv.hidden = true;
-// 	}
-// });
-
-// divBtn.addEventListener('click', () => {
-// 	let newP = document.createElement('p');
-// 	newP.innerHTML = "I'm another P tag";
-// 	divContainer.appendChild(newP);
-// });
-
-// function addItem() {
-// 	let li = document.createElement('li');
-// 	li.innerHTML = "I'm an item";
-// 	ul.appendChild(li);
-// }
--- lecture_09/code/Simple_DOM_Example/script_jq.js
+++ lecture_09/code/Simple_DOM_Example/script_jq.js
@@ -1,14 +1,18 @@
 $('#myForm').submit((event) => {
   event.preventDefault();
-  if ($('#text_input').val()) {
-    $('#error').hide();
+  if ($('#text_input').val().trim()) {
+		$('#error').hide();
+		$('#formLabel').removeClass("error")
+		$('#text_input').removeClass("inputClass")
     const li = `<li> ${$('#text_input').val()} </li>`;
     $('#list').append(li);
     $('#myForm').trigger('reset');
     $('#text_input').focus();
   } else {
     $('#error').show();
-    $('#error').html('You must enter an input value');
+		$('#error').html('You must enter an input value');
+		$('#formLabel').addClass("error")
+		$('#text_input').addClass("inputClass")
     $('#text_input').focus();
   }
 });
